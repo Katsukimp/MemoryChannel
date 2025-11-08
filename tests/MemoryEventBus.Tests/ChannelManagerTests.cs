51134319b6b0072ce185cc47cfc4f0ad95b0cbed
Eduardo Katsuki <eduardokatsuki4@gmail.com>
2025-11-08 18:00:45 -0400

    test: fix component test

--- tests/MemoryEventBus.Tests/ChannelManagerTests.cs
+++ tests/MemoryEventBus.Tests/ChannelManagerTests.cs
@@ -6,41 +6,46 @@ using Xunit;
 
 public class ChannelManagerTests
 {
- private sealed class TestEvent : DomainEvent { }
+    private sealed class TestEvent : DomainEvent { }
 
- [Fact]
- public async Task TryWriteAsync_ShouldReturnFalse_WhenChannelNotCreated()
- {
- var manager = new EventChannelManager();
- var result = await manager.TryWriteAsync(new TestEvent
- {
- EventId = "1",
- EventName = "Test",
- OccurredOn = DateTime.UtcNow
- });
- Assert.False(result);
- }
+    [Fact]
+    public async Task TryWriteAsync_ShouldReturnFalse_WhenChannelNotCreated()
+    {
+        var manager = new EventChannelManager();
+        var result = await manager.TryWriteAsync(new TestEvent
+        {
+            EventId = "1",
+            EventName = "Test",
+            OccurredOn = DateTime.UtcNow
+        });
 
- [Fact]
- public async Task TryWriteAsync_ShouldReturnTrue_AfterChannelCreated()
- {
- var manager = new EventChannelManager();
- manager.GetOrCreateChannel<TestEvent>();
- var result = await manager.TryWriteAsync(new TestEvent
- {
- EventId = "2",
- EventName = "Test",
- OccurredOn = DateTime.UtcNow
- });
- Assert.True(result);
- }
+        Assert.False(result);
+    }
 
- [Fact]
- public void GetChannelDepth_ShouldReturnNonNegative()
- {
- var manager = new EventChannelManager();
- manager.GetOrCreateChannel<TestEvent>();
- var depth = manager.GetChannelDepth<TestEvent>();
- Assert.True(depth >=0);
- }
+    [Fact]
+    public async Task TryWriteAsync_ShouldReturnTrue_AfterChannelCreated()
+    {
+        var manager = new EventChannelManager();
+        manager.GetOrCreateChannel<TestEvent>();
+
+        var result = await manager.TryWriteAsync(new TestEvent
+        {
+            EventId = "2",
+            EventName = "Test",
+            OccurredOn = DateTime.UtcNow
+        });
+
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void GetChannelDepth_ShouldReturnNonNegative()
+    {
+        var manager = new EventChannelManager();
+        manager.GetOrCreateChannel<TestEvent>();
+
+        var depth = manager.GetChannelDepth<TestEvent>();
+
+        Assert.True(depth >=0);
+    }
 }
